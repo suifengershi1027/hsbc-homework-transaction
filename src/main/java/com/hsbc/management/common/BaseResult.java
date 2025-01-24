df119fb7795f945d471e118fe5506913dbcb0bc6
yudongyan <fengoan@kuaishou.com>
2025-01-24 21:24:23 +0800

    init commit

--- src/main/java/com/hsbc/management/common/BaseResult.java
+++ src/main/java/com/hsbc/management/common/BaseResult.java
@@ -1,37 +1,14 @@
 package com.hsbc.management.common;
 
+import lombok.Data;
 
+@Data
 public class BaseResult<T> {
     private int code;
 
     private String msg;
 
     private T data;
-
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
 
     public BaseResult(ResultCodeEnum resultEnum) {
         this.code = resultEnum.getCode();
